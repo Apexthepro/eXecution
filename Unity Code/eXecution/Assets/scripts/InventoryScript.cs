b56e91e3c5021818f01c669d88db0eed61625836
Apexthepro <avishkadakia1996@gmail.com>
2018-06-04 13:25:39 +0530

    code v1.17

---
+++ Unity Code/eXecution/Assets/scripts/InventoryScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+public class InventoryScript : MonoBehaviour {
+    public GameObject myItemsPanel;
+    public GameObject shoppingPanel;
+    public GameObject[] submenulist;
+    public int itemCount;
+    public GameObject CurrentItemClicked;
+    // Use this for initialization
+    
+
+    public void OpenMyItems()
+    {
+        shoppingPanel.SetActive(false);
+        myItemsPanel.SetActive(true);
+    }
+    public void UseItem()
+    {
+        print("Item Used");
+        HideSubmenus();
+        //transform.GetChild(0).transform.GetChild(0).transform.GetChild(0)
+    }
+    public void OpenItemUseMenu(GameObject itemClicked)
+    {
+        CurrentItemClicked = itemClicked;
+        itemCount = Convert.ToInt32(itemClicked.transform.GetChild(0).transform.GetChild(0).transform.GetChild(0).GetComponent<UnityEngine.UI.Text>().text);
+        print("-->Item Count " + itemCount);
+        if (itemCount > 1)
+        {
+            submenulist[1].SetActive(true);//Display multiple item use slide
+
+        }
+        //transform.GetChild(0).transform.GetChild(0).transform.GetChild(0)
+    }
+    public void OpenSubmenu(GameObject Menu) {
+        Menu.SetActive(true);
+    }
+    public void OpenShop()
+    {
+        shoppingPanel.SetActive(true);
+        myItemsPanel.SetActive(false);
+    }
+    public void HideSubmenus() {
+        for (int i = 0; i < submenulist.Length; i++)
+        {
+            submenulist[i].SetActive(false);
+        }
+    }
+}
