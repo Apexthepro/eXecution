91815907cbeca55d0b6bd42b8c1cc7fab2a3be6e
Apexthepro <avishkadakia1996@gmail.com>
2018-06-11 11:38:54 +0530

    code v1.42

---
+++ Unity Code/eXecution/Assets/CitySceneAssets/scripts/Forge/ForgeRecipie.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "New Forge Recipie", menuName = "Forge Recipies")]
+public class ForgeRecipie : ScriptableObject
+{
+
+    public string Name;
+    public Sprite Icon;
+    public string Type;
+    public float[] forgetime;
+    public float[] resourceQuantity;
+    public float[] effectDuration;
+    public int[] XpGained;
+    public string Description;
+
+}
---
+++ Unity Code/eXecution/Assets/CitySceneAssets/scripts/Forge/forgerecipiescript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class forgerecipiescript : MonoBehaviour {
+    public ForgeRecipie ForgeRecipie;
+    public Text Name;
+    public Image Icon;
+    public Text Type;
+    public Text forgetime;
+    public Text resourceQuantity;
+    public Text effectDuration;
+    public Text XpGained;
+    public Text Description;
+    
+    // Use this for initialization
+    void Start () {
+        Name.text = ForgeRecipie.Name;
+        Icon.sprite = ForgeRecipie.Icon;
+        Type.text = ForgeRecipie.Type;
+        forgetime.text = ForgeRecipie.forgetime[0].ToString();
+        resourceQuantity.text = ForgeRecipie.resourceQuantity[0].ToString();
+        XpGained.text = ForgeRecipie.XpGained[0].ToString();
+        Description.text = ForgeRecipie.Description;
+        for (int i = 1; i < ForgeRecipie.forgetime.Length; i++) {
+            forgetime.text += ","+ ForgeRecipie.forgetime[i].ToString();
+            resourceQuantity.text += "," + ForgeRecipie.resourceQuantity[i].ToString();
+            XpGained.text += ","+ForgeRecipie.XpGained[0].ToString();
+        }
+        forgetime.text += "Hours";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
