2387a0faa6122eba0a5660b5da0a5bb9ac7ee9c7
avish <avishkadakia1996@gmail.com>
2018-06-21 05:49:34 +0530

    wrong push but have to do it coz i need to pull

--- Unity Code/eXecution/Assets/GoogleMapScript.cs
+++ Unity Code/eXecution/Assets/GoogleMapScript.cs
@@ -18,7 +18,7 @@ public class GoogleMapScript : MonoBehaviour
     public int scale;
     public int R = 6371;//Radius of earth
     public Image sampleImage;
-    public float x,y,z;
+    public float x, y, z;
     //Vector2 uv = new Vector2((float)myMarker.pixelCoords.x / (float)renderer.material.mainTexture.width, 1f - (float)myMarker.pixelCoords.y / (float)renderer.material.mainTexture.height);
     Vector2 uv;
 
@@ -32,14 +32,10 @@ public class GoogleMapScript : MonoBehaviour
         url = "https://maps.googleapis.com/maps/api/staticmap?center=" + lat + "," + lon +
             "&zoom=" + zoom + "&size=" + 1920 + "x" + 1080 + "&scale=" + scale
             + "&maptype=" + mapSelected +
-<<<<<<< HEAD
             "&markers=color:blue%7Clabel:S%7C1,0&key=AIzaSyAG_VbAh7y2sSRfTRCRlL_ge4EThjjqoRk";
         //url = "https://maps.googleapis.com/maps/api/staticmap?&size=1280x720&style=visibility:on&style=feature:water%7Celement:geometr%7Cvisibility:on&style=feature:landscape%7Celement:geometry%7Cvisibility:on&markers=icon:"+customIcon+"%7CCanberra+ACT&markers=icon:http://tinyurl.com/jrhlvu6%7CMelbourne+VIC+&markers=icon:https://goo.gl/1oTJ9Y%7CSydney+NSW&key=AIzaSyAG_VbAh7y2sSRfTRCRlL_ge4EThjjqoRk";
-=======
-            "&key=AIzaSyAG_VbAh7y2sSRfTRCRlL_ge4EThjjqoRk";
-       // url = "https://maps.googleapis.com/maps/api/staticmap?&size=1280x720&style=visibility:on&style=feature:water%7Celement:geometr%7Cvisibility:on&style=feature:landscape%7Celement:geometry%7Cvisibility:on&markers=icon:"+customIcon+"%7CCanberra+ACT&markers=icon:http://tinyurl.com/jrhlvu6%7CMelbourne+VIC+&markers=icon:https://goo.gl/1oTJ9Y%7CSydney+NSWAIz&key=aSyAG_VbAh7y2sSRfTRCRlL_ge4EThjjqoRk";
->>>>>>> 36cdbd5a57162d5e0e21f0861286b9f11114ac25
-       // url = "https://maps.googleapis.com/maps/api/staticmap?size=512x512&zoom=15&center=Brooklyn&style=feature:road.local%7Celement:geometry%7Ccolor:0x00ff00&style=feature:landscape%7Celement:geometry.fill%7Ccolor:0x000000&style=element:labels%7Cinvert_lightness:true&style=feature:road.arterial%7Celement:labels%7Cinvert_lightness:false&key=AIzaSyAG_VbAh7y2sSRfTRCRlL_ge4EThjjqoRk";
+        // url = "https://maps.googleapis.com/maps/api/staticmap?&size=1280x720&style=visibility:on&style=feature:water%7Celement:geometr%7Cvisibility:on&style=feature:landscape%7Celement:geometry%7Cvisibility:on&markers=icon:"+customIcon+"%7CCanberra+ACT&markers=icon:http://tinyurl.com/jrhlvu6%7CMelbourne+VIC+&markers=icon:https://goo.gl/1oTJ9Y%7CSydney+NSWAIz&key=aSyAG_VbAh7y2sSRfTRCRlL_ge4EThjjqoRk";
+        // url = "https://maps.googleapis.com/maps/api/staticmap?size=512x512&zoom=15&center=Brooklyn&style=feature:road.local%7Celement:geometry%7Ccolor:0x00ff00&style=feature:landscape%7Celement:geometry.fill%7Ccolor:0x000000&style=element:labels%7Cinvert_lightness:true&style=feature:road.arterial%7Celement:labels%7Cinvert_lightness:false&key=AIzaSyAG_VbAh7y2sSRfTRCRlL_ge4EThjjqoRk";
 
 
         loadingMap = true;
@@ -56,14 +52,14 @@ public class GoogleMapScript : MonoBehaviour
         StartCoroutine(mapCoroutine);
         uv = new Vector2((float)608 / (float)MapUtils.LonToX(lon), 1f - (float)350 / (float)MapUtils.LatToY(lat));
         print("Lat = " + lat + "lon = " + lon);
-        
-        x = ((float)(R * Math.Cos(lat) * Math.Cos(lon))/ 6.636458333333333f);
+
+        x = ((float)(R * Math.Cos(lat) * Math.Cos(lon)) / 6.636458333333333f);
 
         y = ((float)(R * Math.Cos(lat) * Math.Sin(lon)) / 11.79814814814815f);
-        print("x =" +x);
-        print("y =" +y);
-      //  print("z =" + R * Math.Sin(lat));
-        sampleImage.transform.position = new Vector3(x,y,0);
+        print("x =" + x);
+        print("y =" + y);
+        //  print("z =" + R * Math.Sin(lat));
+        sampleImage.transform.position = new Vector3(x, y, 0);
 
     }
 
@@ -73,19 +69,17 @@ public class GoogleMapScript : MonoBehaviour
         // print("UvTo3D" + UvTo3D(uv));
         //print("MapUtils lon to x" + MapUtils.LonToX(lon));
         //print("MapUtils lat to y" + MapUtils.LatToY(lat));
-<<<<<<< HEAD
 
-        
-=======
+
+
         //lat = 1;
         //lon = 2;
-        print("Lat = "+ lat +"lon = "+lon);
-        print("x ="+ R * Math.Cos(lat) * Math.Cos(lon));
+        print("Lat = " + lat + "lon = " + lon);
+        print("x =" + R * Math.Cos(lat) * Math.Cos(lon));
         print("y =" + R * Math.Cos(lat) * Math.Sin(lon));
         print("z =" + R * Math.Sin(lat));
->>>>>>> 36cdbd5a57162d5e0e21f0861286b9f11114ac25
-     //   print("long =" + R * Math.Cos(lat) * Math.Cos(lon));
-   //     print("lat =" + R * Math.Cos(lat) * Math.Cos(lon));
+        //   print("long =" + R * Math.Cos(lat) * Math.Cos(lon));
+        //     print("lat =" + R * Math.Cos(lat) * Math.Cos(lon));
 
         if (Input.GetKeyDown(KeyCode.M))
         { //Example of how to update the map with a new set of coordinates
