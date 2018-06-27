533d32733ba252d5a7e5ba450c2b4203a00ee8f9
Apexthepro <avishkadakia1996@gmail.com>
2018-06-27 16:36:22 +0530

    codev1.86

---
+++ Unity Code/eXecution/Assets/tempScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class tempScript : MonoBehaviour, IPointerClickHandler
+{
+    public Animator slideAnimation;
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        print("Entred");
+        slideAnimation.SetBool("OpenMenu", false);
+    }
+
+}
